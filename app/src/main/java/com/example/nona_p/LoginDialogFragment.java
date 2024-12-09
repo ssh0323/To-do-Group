fb77144dbfa92b6f4ff92b50eeb09e294dd0f13b
ssh0323 <tjdgp0132@naver.com>
2024-12-10 02:51:47 +0900

    login pop-up

---
+++ app/src/main/java/com/example/nona_p/LoginDialogFragment.java
@@ -0,0 +1,31 @@
+package com.example.nona_p;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginDialogFragment extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_login, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Dialog 크기 조정
+        if (getDialog() != null && getDialog().getWindow() != null) {
+            getDialog().getWindow().setLayout(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            );
+        }
+    }
+}
--- app/src/main/java/com/example/nona_p/MainActivity.java
+++ app/src/main/java/com/example/nona_p/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.nona_p;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button loginButton = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginDialogFragment loginDialog = new LoginDialogFragment();
+                loginDialog.show(getSupportFragmentManager(), "LoginDialog");
+            }
         });
     }
 }
